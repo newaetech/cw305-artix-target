fc6cdb35165ab67d8797ae9667c4b753fa5424e4
Alex Dewar <adewar@newae.com>
2025-12-11 21:05:15 -0400

    Fix bulk write issue

--- fw/sam3u/CW305_SAM3U_FW/src/config/conf_usb.h
+++ fw/sam3u/CW305_SAM3U_FW/src/config/conf_usb.h
@@ -72,7 +72,7 @@ extern char usb_serial_number[33];
 
 #define FW_VER_MAJOR 0
 #define FW_VER_MINOR 53
-#define FW_VER_DEBUG 0
+#define FW_VER_DEBUG 1
 
 #define  USB_DEVICE_MANUFACTURE_NAME      "NewAE Technology Inc."
 #define  USB_DEVICE_PRODUCT_NAME          "ChipWhisperer CW305"
